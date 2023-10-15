5fd8d6f82569bb74af5f5e4a5489245b3b7af5d8
Lucas Coelho Patrício <lcoelhopatricio@gmail.com>
2023-10-15 11:44:58 -0300

    create index.blade.php

---
+++ resources/views/index.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="refresh" content="0;url={{ route('home') }}">
+</head>
+<body>
+    <!-- Conteúdo adicional, se necessário -->
+</body>
+</html>
